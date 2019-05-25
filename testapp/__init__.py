097c6cd91a1aba1ace4e5b0cdcb5328d24d33698
fk1 <yuichiro@fk1.jp>
2019-05-25 17:45:58 +0900

    postの挙動の確認

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/echo_bot", methods=['POST'])
+def hello_world():
+    return request.data
 
 @app.route("/hoge")
 def hoge():
